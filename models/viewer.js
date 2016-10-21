58ab87a9b3368beedf098108ee7e849a1a6367b3
britbroet <brittanybroetje@gmail.com>
2016-10-21 12:17:24 -0700

    delete and stuff

---
+++ models/viewer.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var viewer = sequelize.define('viewer', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return viewer;
+};
